931510e16661cfad6ff257c28e1535fa20df3a9a
ajgeiss0702 <ajgeiss72@gmail.com>
2022-11-18 14:06:25 -0700

    rest of nav links

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {Divider} from "@brainandbones/skeleton";
+</script>
+
+<br>
+<h1>Contact BASIS Peoria Boosters</h1>
+<br>
+<Divider borderWidth="border-t-4" borderStyle="border-dotted" borderColor="border-tertiary-500"/>
+<br>
+For general Boosters questions, email <a href="president@basispeoriaboosters.org">president@basispeoriaboosters.org</a>.<br>
+<br>
+
+For inquiries regarding the lunch program or lunch orders, email <a href="lunch@basispeoriaboosters.org">lunch@basispeoriaboosters.org</a>.
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import {Divider} from "@brainandbones/skeleton";
+</script>
+
+<br>
+<h1>Links</h1>
+<br>
+<Divider borderWidth="border-t-4" borderStyle="border-dotted" borderColor="border-tertiary-500"/>
+<br>
+<a class="btn btn-lg btn-filled-primary text-black" href="https://smile.amazon.com/ch/45-2095210">
+    Amazon No-Cost Donations
+</a>
+<br>
+<br>
+<a class="btn btn-lg btn-filled-primary text-black" href="https://www.frysfood.com/topic/new-community-rewards-program">
+    Fry’s No-Cost Donations
+</a>
+<br>
+<br>
+<a class="btn btn-lg btn-filled-primary text-black" href="https://parent.smartforcharters.com/">
+    Smart for Charters
+</a>
+<br>
+<br>
+<a class="btn btn-lg btn-filled-primary text-black" href="Smart for Charters">
+    Parent Square
+</a>
+<br>
+<br>
+<a class="btn btn-lg btn-filled-primary text-black" href="http://www.basispeoria.org/">
+    BASIS Peoria Website
+</a>
+<br>
+<br>
+<a class="btn btn-lg btn-filled-primary text-black" href="https://www.basised.com/peoria/events/">
+    BASIS Peoria Event Calendar
+</a>
+<br>
+<br>
+
