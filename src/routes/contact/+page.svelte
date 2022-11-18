5866053bc204ba6c8c74193757ccebcd862e5163
ajgeiss0702 <ajgeiss72@gmail.com>
2022-11-18 16:59:04 -0700

    whoops

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,7 +7,7 @@
 <br>
 <Divider borderWidth="border-t-4" borderStyle="border-dotted" borderColor="border-tertiary-500"/>
 <br>
-For general Boosters questions, email <a href="president@basispeoriaboosters.org">president@basispeoriaboosters.org</a>.<br>
+For general Boosters questions, email <a href="mailto:president@basispeoriaboosters.org">president@basispeoriaboosters.org</a>.<br>
 <br>
 
-For inquiries regarding the lunch program or lunch orders, email <a href="lunch@basispeoriaboosters.org">lunch@basispeoriaboosters.org</a>.
+For inquiries regarding the lunch program or lunch orders, email <a href="mailto:lunch@basispeoriaboosters.org">lunch@basispeoriaboosters.org</a>.
